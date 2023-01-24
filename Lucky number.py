644b4faeaf3033cdbe07c1bab817e16c495939d8
Fernando Muniz <66737379+mooniz@users.noreply.github.com>
2023-01-24 00:39:41 -0300

    Updating
    Thanks to Ju I was able to see some breaches so I worked through fixing them, now you can get a solider lucky number!

--- Lucky number.py
+++ Lucky number.py
@@ -1,4 +1,4 @@
-ASCII_IMAGE = """ Welcome to the fortune teller's tent
+ASCII_IMAGE = """ Welcome to the fortune teller's tent, please touch the ball and don't let it go
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⠾⠛⠋⠉⠉⠉⠉⢙⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⢀⣼⠟⠁⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀
@@ -17,13 +17,27 @@ ASCII_IMAGE = """ Welcome to the fortune teller's tent
 """
 import random
 
+
 def lucky_number(name):
-    number = len(name) * random.randint(1, 1000)
-    print("Hello, " + name + ", your lucky number is " + str(number))
+  number = len(name) * random.randint(1, 1000) // 100
+
+  print("Oh, I see, " + name + "... I'm feeling it, your lucky number is " +
+        str(number))
+
+
 print(ASCII_IMAGE)
 while True:
-    name = input("May I know your name so I can guess your lucky number? ")
-    lucky_number(name)
-    replay = input("Do you want to play again? (yes/no) ")
-    if replay.lower() != "yes":
-        break
+  name = input("May I know your name so I can guess your lucky number? ")
+  lucky_number(name)
+  replay = input("Do you want to play again? (yes/no) ")
+  replay = replay.lower()
+  if replay == "yes":
+    continue
+  elif replay == "no":
+    print("Thank you for coming to my tent, have a nice day!")
+    break
+  else:
+    print(
+      "I see what you're trying to do, but not today! Not here, not now, please type 'yes' or 'no'."
+    )
+    break
